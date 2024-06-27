be8ba03d3d4710a00cc759b9a6a809d2e80212bd
Jason Lalor <jlalor@understood.org>
2024-06-27 15:27:30 -0400

    ui updates: clear chat, swipe up button

--- scripts/frontend.py
+++ scripts/frontend.py
@@ -4,17 +4,29 @@ import asyncio
 
 from chat import create_chatcontroller
 
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+
 @st.cache_resource()
 def first_run():
-  loop = asyncio.new_event_loop()
-  asyncio.set_event_loop(loop)
-  st.session_state["loop"] = loop
   controller, _ = loop.run_until_complete(create_chatcontroller())
   return controller
 
 controller = first_run()
 
-col1, col2, col3 = st.columns([10, 1, 1], vertical_alignment="center")
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = False
+
+def swipe_up():
+    if not st.session_state.clicked:
+      st.session_state.clicked = True
+    else:
+      st.session_state.clicked = False
+
+def clear_chat():
+    controller.History = []
+
+col1, col2, col3, col4 = st.columns([10, 1, 1, 1], vertical_alignment="center")
 
 with col1:
    st.image('images/logo.png')
@@ -24,39 +36,51 @@ with col2:
 
 with col3:
    st.image('images/alpha-icon.png')
+with col4:
+   st.button("\+", on_click=clear_chat)
 
 with st.chat_message("assistant"):
     with st.container(height=200):
       st.header("Hello 👋 I'm Reo. I'm your tutoring buddy. You can use voice or text.")
 
-# left, right = st.columns([1, 11], vertical_alignment="center")
-
-# with left:
-#     st.image('images/mic-icon.png')
-
-# with right:
-#     query = st.chat_input("How can I help you today?", key="query")
-
 left, center, right = st.columns([5, 3, 5], vertical_alignment="center")
 
 with center: 
-  st.image('images/mic-icon.png')
+  icon = st.image('images/mic-icon.png')
 
 with center: 
-  st.write("Swipe up for text input")
+  st.button("Swipe up to type", on_click=swipe_up)
+  # st.write("Swipe up to type")
 
-query = st.chat_input("How can I help you today?", key="query")
+# query = st.chat_input("How can I help you today?", key="query")
 
-if query:
+if st.session_state.clicked:
+  query = st.chat_input("How can I help you today?", key="query")
+
+  if query:
     with st.chat_message("user"):
       st.write(query)
     with st.status("Working on it..."):
       st.write("Searching for data...")
-      loop = st.session_state["loop"]
       inmsg, outmsg = loop.run_until_complete(controller.achat(query))
       st.write("Found data!")
       time.sleep(1)
     with st.chat_message("assistant"):
       st.write(outmsg.Message)
       # st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum non lobortis nisl. In venenatis finibus mauris, et pellentesque nunc egestas id. Integer hendrerit egestas maximus. Ut sed elementum velit. Aenean pulvinar nisl vitae pellentesque condimentum. Donec nec iaculis magna. Donec ornare nisl sed justo euismod, in sagittis quam efficitur.")
+  
+# if query:
+#   with st.chat_message("user"):
+#     st.write(query)
+#   with st.status("Working on it..."):
+#     st.write("Searching for data...")
+#     loop = st.session_state["loop"]
+#     inmsg, outmsg = loop.run_until_complete(controller.achat(query))
+#     st.write("Found data!")
+#     time.sleep(1)
+#   with st.chat_message("assistant"):
+#     st.write(outmsg.Message)
+    # st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum non lobortis nisl. In venenatis finibus mauris, et pellentesque nunc egestas id. Integer hendrerit egestas maximus. Ut sed elementum velit. Aenean pulvinar nisl vitae pellentesque condimentum. Donec nec iaculis magna. Donec ornare nisl sed justo euismod, in sagittis quam efficitur.")
+
+
 
