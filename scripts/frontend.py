e3e254bc815deacb9ae2f16fd525ca4883149dce
Jason Lalor <jlalor@understood.org>
2024-06-27 14:21:27 -0400

    more streamlit updates

--- scripts/frontend.py
+++ scripts/frontend.py
@@ -1,12 +1,29 @@
 import streamlit as st
 import time
 
-st.title("Welcome to the OER Assistant")
+col1, col2, col3 = st.columns([10, 1, 1], vertical_alignment="center")
+
+
+with col1:
+   st.image('images/logo.png')
+
+with col2:
+   st.image('images/flag-icon.png')
+
+with col3:
+   st.image('images/alpha-icon.png')
 
 with st.chat_message("assistant"):
-    st.write("Hello 👋 I'm Reo. I'm here to help you browse OER content. You can use voice or text.")
+    with st.container(height=200):
+      st.header("Hello 👋 I'm Reo. I'm your tutoring buddy. You can use voice or text.")
 
-query = st.chat_input("How can I help you today?", key="query")
+left, right = st.columns([1, 11], vertical_alignment="center")
+
+with left:
+    st.image('images/mic-icon.png')
+
+with right:
+    query = st.chat_input("How can I help you today?", key="query")
 
 if query:
     with st.chat_message("user"):
@@ -19,5 +36,4 @@ if query:
     with st.chat_message("assistant"):
       st.write("Marvelous Magnets: Exploring the Power and Attraction of Magnets")
       st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum non lobortis nisl. In venenatis finibus mauris, et pellentesque nunc egestas id. Integer hendrerit egestas maximus. Ut sed elementum velit. Aenean pulvinar nisl vitae pellentesque condimentum. Donec nec iaculis magna. Donec ornare nisl sed justo euismod, in sagittis quam efficitur.")
-      st.button("Play Audio")
 
